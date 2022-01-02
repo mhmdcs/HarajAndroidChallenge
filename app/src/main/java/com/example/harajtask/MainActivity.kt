c5cad89fa530a7af6d6644be82d396a99910fe8c
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2022-01-02 18:23:39 +0300

    add libraries

--- app/src/main/java/com/example/harajtask/MainActivity.kt
+++ app/src/main/java/com/example/harajtask/MainActivity.kt
@@ -2,7 +2,8 @@ package com.example.harajtask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//https://git.io/JStXV json file
+//use method to convert epoch timestamp to human-readable date https://www.epochconverter.com/
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
