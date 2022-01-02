0cad6fa0f1f264272da9153a64fcab875bbacb0b
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2022-01-02 21:30:54 +0300

    setup classes

--- app/src/main/java/com/example/harajtask/MainActivity.kt
+++ app/src/main/java/com/example/harajtask/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.harajtask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.harajtask.databinding.ActivityMainBinding
+
 //https://git.io/JStXV json file
 //use method to convert epoch timestamp to human-readable date https://www.epochconverter.com/
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        setContentView(binding.root)    }
 }
---
+++ app/src/main/java/com/example/harajtask/overview/OverviewViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.harajtask.overview
+
+class OverviewViewModel {
+}
