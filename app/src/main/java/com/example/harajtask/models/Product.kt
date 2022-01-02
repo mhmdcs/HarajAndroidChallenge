54579942e5f076e4e858a2c571e3c130e8f90c56
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2022-01-02 22:07:02 +0300

    setup network api with retrofit

---
+++ app/src/main/java/com/example/harajtask/models/Product.kt
@@ -0,0 +1,19 @@
+package com.example.harajtask.models
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Entity
+@Parcelize
+data class Product(
+    @PrimaryKey val id: Int,
+    val title: String,
+    val username: String,
+    val thumbURL: String,
+    val commentCount: Int,
+    val city: String,
+    val date: Int,
+    val body: String
+    ): Parcelable
---
+++ app/src/main/java/com/example/harajtask/network/HarajApiService.kt
@@ -0,0 +1,35 @@
+package com.example.harajtask.network
+
+import com.example.harajtask.models.Product
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+//the github shortened file link that points to the forked repo's json file will act as a mock API https://git.io/JStXV
+
+private const val BASE_URL = "https://git.io/"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface HarajApiService {
+
+    @GET("JStXV")
+    suspend fun getProducts(type: String): List<Product>
+
+}
+
+object HarajApi {
+    val retrofitService: HarajApiService by lazy {
+        retrofit.create(HarajApiService::class.java)
+    }
+}
