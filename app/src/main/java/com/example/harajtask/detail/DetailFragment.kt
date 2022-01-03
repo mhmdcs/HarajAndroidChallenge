aa854054868a55f97e5f97815ecb880a0c0bf909
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2022-01-03 18:01:08 +0300

    implement share menu

--- app/src/main/java/com/example/harajtask/detail/DetailFragment.kt
+++ app/src/main/java/com/example/harajtask/detail/DetailFragment.kt
@@ -1,10 +1,9 @@
 package com.example.harajtask.detail
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.example.harajtask.R
 import com.example.harajtask.databinding.FragmentDetailBinding
@@ -26,6 +25,23 @@ class DetailFragment : Fragment() {
         val product = DetailFragmentArgs.fromBundle(requireArguments()).selectedProduct
         binding.product = product
 
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.share_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+       //intent.putExtra(Intent.EXTRA_SUBJECT, "mock data")
+       //intent.putExtra(Intent.EXTRA_TEXT, "mock data")
+        startActivity(Intent.createChooser(intent, "Share"))
+        return super.onOptionsItemSelected(item)
     }
 }
